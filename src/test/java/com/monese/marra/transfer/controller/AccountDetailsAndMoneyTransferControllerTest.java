32c244c4c0d4894d690016a5e5224c86b35032ea
Gerardo Marra <gmarra@us.ibm.com>
2019-06-11 14:42:54 +0100

    refining e2e test case

--- src/test/java/com/monese/marra/transfer/controller/AccountDetailsAndMoneyTransferControllerTest.java
+++ src/test/java/com/monese/marra/transfer/controller/AccountDetailsAndMoneyTransferControllerTest.java
@@ -1,6 +1,7 @@
 package com.monese.marra.transfer.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,10 +44,39 @@ public class AccountDetailsAndMoneyTransferControllerTest {
 	
 	@Test
 	public void verifyAccountBalanceAfterMoneyTransfer() throws Exception{
+		
+		//verify account 11111111 contains 300 GBP
 		ResultActions retrieveBalanceResponse = this.mockMvc.perform(get(ACCOUNT_DETAILS_CONTEXT_PATH).
 				param("accountNumber","11111111").accept(MediaType.parseMediaType(MEDIA_TYPE)));
 		retrieveBalanceResponse.andExpect(content().contentType(MEDIA_TYPE)).
-		andExpect(jsonPath("$.balance").value(310.0));
+		andExpect(jsonPath("$.balance").value(300.0));
+		
+		//verify account 22222222 contains 200 GBP
+		retrieveBalanceResponse = this.mockMvc.perform(get(ACCOUNT_DETAILS_CONTEXT_PATH).
+				param("accountNumber","22222222").accept(MediaType.parseMediaType(MEDIA_TYPE)));
+		retrieveBalanceResponse.andExpect(content().contentType(MEDIA_TYPE)).
+		andExpect(jsonPath("$.balance").value(200.0));
+				
+		
+		//perform 25 GBP transfer between 11111111 and 22222222
+		ResultActions moneyTransferResponseActions = this.mockMvc.perform(post(MONEY_TRANSFER_CONTEXT_PATH).
+				contentType(MediaType.APPLICATION_JSON_UTF8).content(jsonForMoneyTransfer));
+		moneyTransferResponseActions.andExpect(status().isOk());
+		
+		//verify account 11111111 contains 275 GBP
+		retrieveBalanceResponse = this.mockMvc.perform(get(ACCOUNT_DETAILS_CONTEXT_PATH).
+				param("accountNumber","11111111").accept(MediaType.parseMediaType(MEDIA_TYPE)));
+		retrieveBalanceResponse.andExpect(content().contentType(MEDIA_TYPE)).
+		andExpect(jsonPath("$.balance").value(275.0));
+		
+		//verify account 22222222 contains 225 GBP
+		retrieveBalanceResponse = this.mockMvc.perform(get(ACCOUNT_DETAILS_CONTEXT_PATH).
+				param("accountNumber","22222222").accept(MediaType.parseMediaType(MEDIA_TYPE)));
+		retrieveBalanceResponse.andExpect(content().contentType(MEDIA_TYPE)).
+		andExpect(jsonPath("$.balance").value(225.0));
+				
+		
+		
 		
 	}
 
